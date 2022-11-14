26d14b14da066b901ce1605fb5f076fed25da82a
Summer <swatson9008@yahoo.com>
2022-11-14 17:55:48 -0500

    inputting testing code

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 
+let title;
+let author;
+let pages;
+let read;
+
+let submitB = document.getElementById("submitB")
+let titleB = document.getElementById("bookT")
 
 let myLibrary = [];
 
-function book() {
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
 }
 
-function addBookToLibrary () {
+
+submitB.addEventListener ("click", function addBookToLibrary(event){
+    event.preventDefault();
+    console.log("testing");
+    console.log(titleB.value);
     
-}
+
+});
+
+
+
+/*function addBookToLibrary (event) {
+    event.preventDefault();
+    console.log("testing");
+
+
+
+}*/
