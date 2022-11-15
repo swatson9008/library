5f761361451429803970e1ae1320706152bde427
Summer <swatson9008@yahoo.com>
2022-11-15 15:53:13 -0500

    changes

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ let title;
 let author;
 let pages;
 let read;
+let unread
+let complete
+let readC
+
+
 
 let submitB = document.getElementById("submitB")
 let titleB = document.getElementById("bookT")
 let authorB = document.getElementById("authorName")
 let pageB = document.getElementById("pagesNo")
+let readB = document.getElementById("readConfirm")
+
+let bookArea = document.getElementById("bookArea")
+let bookTArea = document.getElementById("bookTitles")
 
 let myLibrary = [];
+
+let sectionCreate = document.createElement("div")
+
 
 function book(title, author, pages, read) {
     this.title = title;
@@ -26,21 +38,25 @@ submitB.addEventListener ("click", function addBookToLibrary(event){
     newBook.title = titleB.value;
     newBook.author = authorB.value;
     newBook.pages = pageB.value;
-    console.log(newBook.title);
-    myLibrary = Object.values(newBook);
+    newBook.read = readB.checked;
+    myLibrary.push(newBook);
     console.log(myLibrary);
+    titleB.value = authorB.value = pageB.value = "";
+    bookUpdate();
+    
 
     
-    
-
 });
 
+function bookUpdate(){
+    myLibrary.forEach(element => {
+        sectionCreate.class = "newBooks";
+        sectionCreate.innerHTML = 
+            "<h4>Title:</h4>" + element.title;
+        
 
+    });
+    
 
-/*function addBookToLibrary (event) {
-    event.preventDefault();
-    console.log("testing");
-
-
-
-}*/
+    bookArea.appendChild(sectionCreate)
+}
